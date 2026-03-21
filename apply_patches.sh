e3af7eeb569234454637aa5a560305fe16c9ff27
minerjed <az0006t@protonmail.com>
2026-03-21 08:12:17 -0400

    Remove monero tag checkout from apply_patches.sh

--- apply_patches.sh
+++ apply_patches.sh
@@ -68,11 +68,6 @@ fi
 git submodule init
 git submodule update --init --recursive --force
 
-if [[ "$repo" == "monero" ]]; then
-    git fetch --tags
-    git checkout "v0.18.3.4-beta2"
-fi
-
 # Only operate on files tracked by THIS repo, not nested submodule contents
 while IFS= read -r file; do
     [[ -f "$file" ]] || continue
